44f2428792cc32f79a7880f6cf5adfe3ebdd0975
Soham Chakraborty <schakraborty8455@gmail.com>
2022-10-29 22:52:49 +0530

    Database created and Bootstrap loaded for frontend purpose

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    sNo = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(2000), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self)->str:
+        return f"{self.sNo}-{self.title}"
+
+
+@app.route('/')
+def hello_world():
+    query1= Todo(title="Next task", desc="Attend scalar academy master class")
+    db.session.add(query1)
+    db.session.commit()
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
